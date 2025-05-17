a74f51878b6fa5f8f76fc8be5d6f0eafcb16f9ba
Nabeel Hussain <mnnh2001@gmail.com>
2025-05-17 11:27:36 +0530

    Added solution for 2015 Day04 puzzle

---
+++ 2015/day-04/Part2.java
@@ -0,0 +1,33 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Part2 {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        String input = "ckczppom";
+        boolean resultFound = false;
+
+        int saltNumber = 1;
+
+        while (!resultFound) {
+            StringBuilder secretKey = new StringBuilder(input);
+            secretKey.append(saltNumber);
+
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(secretKey.toString().getBytes());
+            BigInteger num = new BigInteger(1, messageDigest);
+            String hashText = num.toString(16);
+
+            while (hashText.length() < 32) {
+                hashText = "0" + hashText;
+            }
+
+            if (hashText.startsWith("000000"))
+                resultFound = true;
+            else
+                saltNumber++;
+        }
+
+        System.out.println(saltNumber);
+    }
+}
